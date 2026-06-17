566932a4954d2b5589b2e0d8c51e79409c9098f0
Joshua <jaboytes.sebastian@gmail.com>
2026-06-17 15:19:43 -0600

    Conexion MongoDB y Express server

---
+++ backend/src/config/database.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (): Promise<void> => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string);
+
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB");
+        console.error(error);
+
+        process.exit(1);
+    }
+};
